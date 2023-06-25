0ab941fdf5b1f5424fbcced7fe03410d78af83e4
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2023-06-25 12:28:37 +0530

    comments added

--- src/main.js
+++ src/main.js
@@ -5,44 +5,50 @@ let timer = null;
 
 
 
-
+//the Timer with Hour Min and Sec 
 const changeTime = function(){
+    //increaseing the seconds
     sec++;
     if(sec===60){
         sec=0;
+        //increase the minutes
         min++;
         if(min===60){
             min=0;
+            //increase the hours
             hour++;
         }
     }
+    //fill the timer with 0 if min is between 1 to 9
     let h = hour < 10 ? '0'+hour : hour;
     let m = min < 10 ? '0'+min : min;
     let s = sec < 10 ? '0'+sec : sec;
     displayTime.innerText = h + ":" + m + ":" + s ;
 }
-    
+
+// three buttons for the working of stopwatch
 let startButton = document.getElementById("play");
 let stopButton = document.getElementById("stop");
 let resetButton = document.getElementById("reset");
 
+//Function to start the timer
 function startWatch(){
     if(timer!==null){
         clearInterval(timer);
     }
     timer = setInterval(changeTime,1000);
 }
-
+//Function to stop the Stopwatch timer
 function stopWatch(){
     clearInterval(timer);
 }
-
+//reset timer Function changes the timer to 00:00:00
 function resetWatch(){
     clearInterval(timer);
     [sec,min,hour]=[0,0,0];
     displayTime.innerText = "00:00:00";
 }
-    
+//Event handlers for the timer events using the addEventListener method    
 startButton.addEventListener("click",startWatch);
 stopButton.addEventListener("click",stopWatch);
 resetButton.addEventListener("click",resetWatch);
